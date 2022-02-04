3022ecfb2ff5cf4d3139144ad9fa2a167a56dcfe
odikil <pedro.donoso.r@protonmail.com>
2022-02-04 16:13:40 -0300

    get y post funcionan

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -9,10 +9,18 @@ const pool = new Pool({
 });
 
 const getUsers = async (req, res) => {
-   const response = await pool.query('SELECT * FROM users');
+    const response = await pool.query('SELECT * FROM users');
     res.status(200).json(response.rows);
-}
+};
+
+const createUser = async (req, res) => {
+    const { name, email } = req.body;
+    const response = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
+    console.log(response);
+    res.send('user created');
+};
 
 module.exports = {
-    getUsers
+    getUsers,
+    createUser
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getUsers} = require('../controllers/index.controller')
+const { getUsers, createUser} = require('../controllers/index.controller')
 
-router.get('/users', getUsers)
+router.get('/users', getUsers);
+router.post('/users', createUser);
 
 module.exports = router;
