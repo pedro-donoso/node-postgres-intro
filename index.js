0d289c1e469984bf461359e2adc8c081da4ebd99
odikil <pedro.donoso.r@protonmail.com>
2022-02-04 12:16:00 -0300

    inserto nuevo usuario

--- index.js
+++ index.js
@@ -11,13 +11,26 @@ const pool = new Pool(config);
 
 const getBooks = async () => {
     try {
-        const res = await pool.query('select * from books');
+        const res = await pool.query('select * from users');
         console.log(res.rows);
         pool.end();
     } catch(e) {
         console.log(e);
     }
 };
-getBooks();
 
-// ejemplo
+const insertUser = async () => {
+   try {
+        const text = 'INSERT INTO users(username, password) VALUES ($1, $2)'
+        const values = ['jhon', 'john1234']
+
+        const res = await pool.query(text, values);
+        console.log(res);
+        pool.end();
+   } catch (e) {
+       console.log(e);
+   }
+}
+
+insertUser();
+
