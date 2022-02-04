ec8a8f0f44183c2cf4de1d827639cd503c40cb8d
odikil <pedro.donoso.r@protonmail.com>
2022-02-04 15:33:14 -0300

    metodo get funciona

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,7 @@
+const getUsers = (req, res) => {
+    res.send('users');
+}
+
+module.exports = {
+    getUsers
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/users', (req, res)=>{
-    res.send('users');
-})
+const { getUsers} = require('../controllers/index.controller')
+
+router.get('/users', getUsers)
 
 module.exports = router;
